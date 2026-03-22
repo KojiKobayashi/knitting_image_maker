588aed9cb36fe1a6ae2717164c60358e6a16dfc4
KojiKobayashi <k.kobayashi@sansan.com>
2026-03-22 22:54:28 +0900

    repositry name

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/csv2image_js/',
+  base: '/knitting_image_maker/',
   plugins: [react()],
 })
